4193c9211089451ebf5a2420f23b711a6e3dffb1
Labib666Camp <mainulislamlabib@gmail.com>
2026-02-08 02:26:59 +0600

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+//   base: '/urbor.earth/',
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,5 +20,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/urbor.earth/',
+  base: '/', // Change this from '/urbor.earth/' to '/'
 })
